51e1c154f8d21939dc1a27b0334551b65f6d0a79
Macarena Aviles <esperanzamacarenaaviles@gmail.com>
2022-08-22 09:06:41 +0200

    chore: creation of first classes

---
+++ app/src/test/java/DataBaseTest.java
@@ -0,0 +1,10 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DataBaseTest {
+    @Test
+    void when_add_adding_an_element_to_hashmap_then_return_element(){
+
+        Assertions.assertThat();
+    }
+}

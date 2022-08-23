0e97e920ee7b416cee673584536ad18da1656e85
Macarena Aviles <esperanzamacarenaaviles@gmail.com>
2022-08-23 17:12:18 +0200

    chore: refactor AccountRepositoryTest

--- app/src/test/java/AccountRepositoryTest.java
+++ app/src/test/java/AccountRepositoryTest.java
@@ -1,21 +1,25 @@
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import java.util.Date;
-
 import java.util.ArrayList;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class AccountRepositoryTest {
 
     @Test
-    void save_method_from_account_repository_works() {
+    void check_that_save_method_from_account_repository_works() {
+        assertThat(createActualData()).usingRecursiveComparison().isEqualTo(createExpectedData());
+    }
+
+    private ArrayList<Transaction> createActualData() {
         AccountRepository accountRepository = new AccountRepository();
         accountRepository.save("hora", 100);
         ArrayList<Transaction> actualData = accountRepository.findAll();
+        return actualData;
+    }
+
+    private ArrayList<Transaction> createExpectedData() {
         ArrayList<Transaction> expectedData = new ArrayList<>();
-        Integer amount = 100;
         Transaction transaction = new Transaction("hora", 100);
         expectedData.add(transaction);
-        assertThat(actualData).usingRecursiveComparison().isEqualTo(expectedData);
+        return expectedData;
     }
 }

f0a2eb1de248b23f6d891f3c6131783ed01b3ea2
Macarena Aviles <esperanzamacarenaaviles@gmail.com>
2022-08-23 22:18:42 +0200

    chore: refactor test code for StatementGeneratorTest

--- app/src/test/java/StatementGeneratorTest.java
+++ app/src/test/java/StatementGeneratorTest.java
@@ -8,6 +8,10 @@ public class StatementGeneratorTest {
 
     @Test
     void check_that_get_data_method_works() {
+        assertThat(createActualResult()).usingRecursiveComparison().isEqualTo(createExpectedResult());
+    }
+
+    private ArrayList<TransactionForStatement> createActualResult() {
         DataBase database = new DataBase();
         Transaction transaction1 = new Transaction("25-06-2022", 300);
         Transaction transaction2 = new Transaction("01-11-2022", -200);
@@ -15,13 +19,15 @@ public class StatementGeneratorTest {
         database.data.add(transaction2);
         StatementGenerator statementGenerator = new StatementGenerator(database);
         ArrayList<TransactionForStatement> actualResult = statementGenerator.getData();
+        return actualResult;
+    }
 
+    private ArrayList<TransactionForStatement> createExpectedResult() {
         ArrayList<TransactionForStatement> expectedResult = new ArrayList<>();
         TransactionForStatement transactionForStatement1 = new TransactionForStatement("25-06-2022", 300, 300);
         TransactionForStatement transactionForStatement2 = new TransactionForStatement("01-11-2022", -200, 100);
         expectedResult.add(transactionForStatement1);
         expectedResult.add(transactionForStatement2);
-
-        assertThat(actualResult).usingRecursiveComparison().isEqualTo(expectedResult);
+        return expectedResult;
     }
 }

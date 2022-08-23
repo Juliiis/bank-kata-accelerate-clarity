820191478c4aef7aa5c73a0202b0c2998abd6182
Macarena Aviles <esperanzamacarenaaviles@gmail.com>
2022-08-23 17:17:33 +0200

    chore: refactor when_adding_an_element_to_list_then_return_element test

--- app/src/test/java/DataBaseTest.java
+++ app/src/test/java/DataBaseTest.java
@@ -1,15 +1,19 @@
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 public class DataBaseTest {
     @Test
-    void when_adding_an_element_to_hashmap_then_return_element(){
-        DataBase database = new DataBase();
-        database.data.put("timestamp", 100);
-        Integer actualResult = database.data.size();
+    void when_adding_an_element_to_list_then_return_element(){
         Integer expectedResult = 1;
-        Assertions.assertThat(actualResult).isEqualTo(expectedResult);
+        Assertions.assertThat(createActualResult()).isEqualTo(expectedResult);
+    }
+
+    private Integer createActualResult(){
+        DataBase database = new DataBase();
+        Transaction transaction = new Transaction("timestamp", 100);
+        database.data.add(transaction);
+        Integer actualResult = database.data.size();
+        return actualResult;
     }
 }
 

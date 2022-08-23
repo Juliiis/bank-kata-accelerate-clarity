3506afeab0873648989d373a0eca0487fe86b87a
Macarena Aviles <esperanzamacarenaaviles@gmail.com>
2022-08-23 22:16:14 +0200

    chore: add check_that_get_data_method_works test for StatementGeneratorTest

---
+++ app/src/test/java/StatementGeneratorTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class StatementGeneratorTest {
+
+    @Test
+    void check_that_get_data_method_works() {
+        DataBase database = new DataBase();
+        Transaction transaction1 = new Transaction("25-06-2022", 300);
+        Transaction transaction2 = new Transaction("01-11-2022", -200);
+        database.data.add(transaction1);
+        database.data.add(transaction2);
+        StatementGenerator statementGenerator = new StatementGenerator(database);
+        ArrayList<TransactionForStatement> actualResult = statementGenerator.getData();
+
+        ArrayList<TransactionForStatement> expectedResult = new ArrayList<>();
+        TransactionForStatement transactionForStatement1 = new TransactionForStatement("25-06-2022", 300, 300);
+        TransactionForStatement transactionForStatement2 = new TransactionForStatement("01-11-2022", -200, 100);
+        expectedResult.add(transactionForStatement1);
+        expectedResult.add(transactionForStatement2);
+
+        assertThat(actualResult).usingRecursiveComparison().isEqualTo(expectedResult);
+    }
+}

03a40feb6be20a1b2df1ff12fa887292169e5100
Macarena Aviles <esperanzamacarenaaviles@gmail.com>
2022-08-24 18:35:56 +0200

    chore: add check_that_print_statement_method_works test

--- app/src/test/java/AccountTest.java
+++ app/src/test/java/AccountTest.java
@@ -31,4 +31,18 @@ public class AccountTest {
         expectedTransactionList.add(transaction);
         assertThat(actualTransactionList).usingRecursiveComparison().isEqualTo(expectedTransactionList);
     }
+
+    @Test
+    void check_that_print_statement_method_works() {
+        AccountRepository accountRepository = new AccountRepository();
+        Account account = new Account(accountRepository);
+        account.deposit(300);
+        account.withdraw(100);
+        account.deposit(300);
+        StatementGenerator statementGenerator = new StatementGenerator(accountRepository.database);
+        String actualOutputStatement = account.printStatement(statementGenerator);
+
+        String expectedOutputStatement = "        Date      Amount     Balance\n  04-07-2022         300         300\n  05-07-2022        -100         200\n  04-07-2022         300         500\n";
+        assertThat(actualOutputStatement).isEqualTo(expectedOutputStatement);
+    }
 }

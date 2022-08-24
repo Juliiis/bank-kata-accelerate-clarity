c870f6b309f9f1397e6bd05c11ca956b4d103a02
Macarena Aviles <esperanzamacarenaaviles@gmail.com>
2022-08-24 16:16:18 +0200

    chore: add test check_that_deposit_method_works

--- app/src/main/java/Account.java
+++ app/src/main/java/Account.java
@@ -7,11 +7,24 @@ public class Account {
     }
 
     void deposit(int amount){
-
+        if (amount > 0) {
+            Transaction transaction = new Transaction("04-07-2022", amount);
+            accountRepository.save(transaction);
+        }
+        else {
+            throw new RuntimeException("The amount must be a positive number.");
+        }
     }
 
     void withdraw(int amount){
-
+        if (amount > 0) {
+            amount *= -1;
+            Transaction transaction = new Transaction("05-07-2022", amount);
+            accountRepository.save(transaction);
+        }
+        else {
+            throw new RuntimeException("The amount must be a positive number.");
+        }
     }
 
     String printStatement(StatementGenerator statementGenerator){
--- app/src/test/java/AccountTest.java
+++ app/src/test/java/AccountTest.java
@@ -1,5 +1,21 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class AccountTest {
 
+    @Test
+    void check_that_deposit_method_works() {
+        AccountRepository accountRepository = new AccountRepository();
+        Account account = new Account(accountRepository);
+        account.deposit(100);
+        ArrayList<Transaction> actualTransactionList = accountRepository.findAll();
+
+        ArrayList<Transaction> expectedTransactionList = new ArrayList<>();
+        Transaction transaction = new Transaction("04-07-2022", 100);
+        expectedTransactionList.add(transaction);
+        assertThat(actualTransactionList).usingRecursiveComparison().isEqualTo(expectedTransactionList);
+    }
 }

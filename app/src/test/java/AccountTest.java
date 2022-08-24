2a67a87599fc449092f6ef9ff58b701256209d1e
Macarena Aviles <esperanzamacarenaaviles@gmail.com>
2022-08-24 16:28:22 +0200

    chore: add test check_that_withdraw_method_works

--- app/src/test/java/AccountTest.java
+++ app/src/test/java/AccountTest.java
@@ -18,4 +18,17 @@ public class AccountTest {
         expectedTransactionList.add(transaction);
         assertThat(actualTransactionList).usingRecursiveComparison().isEqualTo(expectedTransactionList);
     }
+    
+    @Test
+    void check_that_withdraw_method_works() {
+        AccountRepository accountRepository = new AccountRepository();
+        Account account = new Account(accountRepository);
+        account.withdraw(50);
+        ArrayList<Transaction> actualTransactionList = accountRepository.findAll();
+
+        ArrayList<Transaction> expectedTransactionList = new ArrayList<>();
+        Transaction transaction = new Transaction("05-07-2022", -50);
+        expectedTransactionList.add(transaction);
+        assertThat(actualTransactionList).usingRecursiveComparison().isEqualTo(expectedTransactionList);
+    }
 }
